5f4c9dd2df11b5b3af5e451a312be6459fed0be0
João Alves <njoao97710@gmail.com>
2021-04-19 21:29:05 +0000

    :hammer: CHORE(document): remove import font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -62,10 +62,6 @@ export default class MyDocument extends Document {
 
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          />
         </Head>
         <body>
           <Main />
