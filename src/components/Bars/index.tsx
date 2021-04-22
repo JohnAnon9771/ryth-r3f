305305ebb9a1d3137d88172e3c80f760b7ba059a
João Alves <njoao97710@gmail.com>
2021-04-22 00:56:29 +0000

    :sparkles: FEAT(Bars): add animation for bars when change page

--- src/components/Bars/index.tsx
+++ src/components/Bars/index.tsx
@@ -1,4 +1,4 @@
-import {memo} from 'react'
+import {useRouter} from 'next/router'
 
 import {animated, useSpring} from '@react-spring/web'
 
@@ -6,14 +6,21 @@ interface Props {
   position: 'top' | 'bottom'
 }
 
-function Bars({position}: Props): JSX.Element {
+export default function Bars({position}: Props): JSX.Element {
+  const router = useRouter()
   const {transform} = useSpring({
-    transform: position === 'top' ? 'translateY(-285px)' : 'translateY(285px)',
+    transform:
+      position === 'top'
+        ? router.route !== '/'
+          ? 'translateY(calc(-50vh * 1))'
+          : 'translateY(calc(-50vh * 0.8))'
+        : router.route !== '/'
+        ? 'translateY(calc(50vh * 1))'
+        : 'translateY(calc(50vh * 0.8))',
     from: {
       transform: 'translateY(0px)',
     },
   })
+
   return <animated.div className={`bar ${position}`} style={{transform}} />
 }
-
-export default memo(Bars)
