a02761caf087d2350c25830d8c402caffa9fa6f3
João Alves <njoao97710@gmail.com>
2021-04-23 18:18:14 +0000

    :sparkles: FEAT: add condition for home page

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,26 +1,45 @@
 import Link from 'next/link'
-import {useRouter} from 'next/router'
 
 import * as S from './style'
 
-export default function Navigation(): JSX.Element {
-  const router = useRouter()
+interface Props {
+  isHome: boolean
+}
+
+export default function Navigation({isHome}: Props): JSX.Element {
   return (
-    <S.Container>
-      {router.route !== '/' && (
-        <Link href="/">
-          <a>Home</a>
-        </Link>
+    <>
+      {isHome ? (
+        <S.NavigationHome>
+          <Link href="/work">
+            <a>Work</a>
+          </Link>
+          <Link href="/info">
+            <a>Info</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </S.NavigationHome>
+      ) : (
+        <S.NavigationOtherPages>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <span>|</span>
+          <Link href="/work">
+            <a>Work</a>
+          </Link>
+          <span>|</span>
+          <Link href="/info">
+            <a>Info</a>
+          </Link>
+          <span>|</span>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </S.NavigationOtherPages>
       )}
-      <Link href="/work">
-        <a>Work</a>
-      </Link>
-      <Link href="/info">
-        <a>Info</a>
-      </Link>
-      <Link href="/blog">
-        <a>Blog</a>
-      </Link>
-    </S.Container>
+    </>
   )
 }
